47d2b0b78cf2e2faf4b2c8f947faad87dc8360ea
Prince Kumar <princek9576@gmail.com>
2021-07-05 23:44:32 +0530

    Address form styling added

--- src/components/Cart/AddressForm.js
+++ src/components/Cart/AddressForm.js
@@ -2,6 +2,11 @@ import Button from "../UI/Button/Button";
 import styles from "./AddressForm.module.css";
 
 const AddressForm = (props) => {
+  const importantMarkup = (
+    <sup className={styles.important}>
+      <i className="fas fa-asterisk"></i>
+    </sup>
+  );
   const addAddressHandler = (event) => {
     event.preventDefault();
     console.log("Add Address Handler");
@@ -15,6 +20,7 @@ const AddressForm = (props) => {
         </div>
         <div className={styles["form-control"]}>
           <label htmlFor="apartment">Building/Apartment Name</label>
+          {importantMarkup}
           <input
             type="text"
             id="apartment"
@@ -26,6 +32,7 @@ const AddressForm = (props) => {
       <div className={`${styles["form-group"]} ${styles.two}`}>
         <div className={styles["form-control"]}>
           <label htmlFor="street">Street Number</label>
+          {importantMarkup}
           <input type="number" id="street" placeholder="Ex : 102"></input>
         </div>
         <div className={styles["form-control"]}>
@@ -37,24 +44,30 @@ const AddressForm = (props) => {
       <div className={`${styles["form-group"]} ${styles.three}`}>
         <div className={styles["form-control"]}>
           <label htmlFor="city">City</label>
+          {importantMarkup}
           <input type="text" id="city" placeholder="Ex : Mumbai"></input>
         </div>
         <div className={styles["form-control"]}>
           <label htmlFor="state">State</label>
+          {importantMarkup}
           <input type="text" id="state" placeholder="Ex : Maharashtra"></input>
         </div>
         <div className={styles["form-control"]}>
           <label htmlFor="zip">Zip</label>
+          {importantMarkup}
           <input type="number" id="zip" placeholder="Ex : 829440"></input>
         </div>
       </div>
-
+      <div className={styles.indicator}>
+        {importantMarkup} fields are required.
+      </div>
       <div className={styles["form-action"]}>
         <Button
+          className={styles["btn-margin"]}
           config={{
             width: "100%",
-            color: "dodgerblue",
-            padding: "15px",
+            color: "rgb(4, 170, 109)",
+            padding: "19px",
           }}
           type="submit"
           name="Submit"
